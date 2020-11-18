24cae3e66caadbe3eb6a66fa3e48f51a39647af8
chyppoli <claude.hyppolite@gmail.com>
2020-11-17 19:00:47 -0500

    add apiroutes and htmlroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,23 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
+const noteData = require("../db/db");
+const fs = require("fs");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
   });
 
   // API POST Requests
@@ -35,15 +28,14 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/notes", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     if (tableData.length < 5) {
       tableData.push(req.body);
       res.json(true);
-    }
-    else {
+    } else {
       waitListData.push(req.body);
       res.json(false);
     }
@@ -53,7 +45,7 @@ module.exports = function(app) {
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
