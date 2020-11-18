7027c9f8ca87480a9a8fe52f12ccdbef0939650e
chyppoli <claude.hyppolite@gmail.com>
2020-11-18 15:16:44 -0500

    clean up apiRoutes file..remove comments and unused lines

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,14 +10,9 @@ const arrayNotesCurrent = JSON.parse(fs.readFileSync(db));
 // ===============================================================================
 
 module.exports = function (app) {
-  // const db = "./db/db.json";
-
-  let arrayNotesNew = [];
+  // let arrayNotesNew = [];
 
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function (req, res) {
@@ -26,44 +21,26 @@ module.exports = function (app) {
   });
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/notes", function (req, res) {
     let objNewNote = req.body;
-    // let arrayNotesCurrent = [];
-
-    // let arrayNotesCurrent = JSON.parse(fs.readFileSync(db));
     let nextId = arrayNotesCurrent.length + 1;
     objNewNote.id = nextId;
 
-    // let arrayNotesNew = arrayNotesCurrent.push(objNewNote);
-    //   if (err) throw err;
-
-    //   arrayNotesCurrent = data;
     let arrayNotesNew = arrayNotesCurrent;
 
     arrayNotesNew.push(objNewNote);
 
-    //   let newData = data.replace(/.*/, arrayNotesNew);
-
     fs.writeFileSync(db, JSON.stringify(arrayNotesNew, null, "\t"));
-    // });
 
-    // console.log(objNewNote);
-    // console.log(arrayNotesCurrent);
     console.log(arrayNotesNew);
 
     res.send(objNewNote);
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  // API Delete Route
 
   app.delete("/api/notes/:id", function (req, res) {
     let newNotesArray = arrayNotesCurrent.filter(function (value, index, arr) {
@@ -71,15 +48,11 @@ module.exports = function (app) {
     });
 
     fs.writeFileSync(db, JSON.stringify(newNotesArray, null, "\t"));
-    // res.send(
-    //   `Got a Delete Request at /${req.params.id} \n ${JSON.parse(
-    //     newNotesArray
-    //   )}`
-    // );
-    // res.send(newNotesArray);
+
     console.log(arrayNotesCurrent);
     console.log(newNotesArray);
     console.log(req.params.id);
+
     //res.send("received a delete request");
     res.json({ ok: true });
   });
